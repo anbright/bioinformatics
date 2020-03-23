7b4d39301a79717a88d35f123f2cf8c281c40a59
Andrew Bright <anbright96@gmail.com>
2020-03-23 08:56:01 -0700

    Add reverse_complement.py

--- kmer_count.py
+++ kmer_count.py
@@ -5,12 +5,14 @@ def pattern_count(input: str, pattern: str) -> int:
     i = 0
     count = 0
     k = len(pattern)
+    indices = []
     pattern_length = len(input) - k + 1
     for i in range(pattern_length):
         substr = input[i : i + len(pattern)]
         if pattern == substr:
             count += 1
-    return count
+            indices.append(i)
+    return (count, indices)
 
 
 def count_kmers(input: str, k: int) -> List[int]:
@@ -27,7 +29,7 @@ def count_kmers(input: str, k: int) -> List[int]:
     search_length = len(input) - k + 1
     for i in range(search_length):
         pattern = input[i : i + k]
-        counts[i] = pattern_count(input, pattern)
+        counts[i], _ = pattern_count(input, pattern)
         max_count = max(max_count, counts[i])
 
     frequent_patterns = set()
@@ -46,3 +48,5 @@ if __name__ == "__main__":
 
     print(kmer_set)
     print(" ".join(list(kmer_set)))
+
+    print(pattern_count("GATATATGCATATACTT", "ATAT",))
